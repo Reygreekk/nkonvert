62940ab92c657bc1102b46eda23e716a4e0d4ce4
Reygreekk <93857141+Reygreekk@users.noreply.github.com>
2026-01-16 04:31:08 +0000

    app.py

--- app.py
+++ app.py
@@ -196,7 +196,7 @@ def generate_boost():
         },
          "C'est toi": {
             "sujets": ["Ton autorité"],
-            "actions": [""se façonne quand tu"],
+            "actions": ["se façonne quand tu"],
             "finalites": ["admets et apprends tes échecs."]
         },
         "C'est le moment d'écrire ton histoire": {
@@ -319,6 +319,7 @@ def download_file(filename):
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+
 
 
 
